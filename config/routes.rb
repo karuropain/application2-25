066e1bc697f3c2d76e821a09f2aba365482b77af
karuropain <fs.sai5041@gmail.com>
2022-05-12 10:28:56 +0000

    Update レイアウト作成から

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  get 'books/index'
-  get 'books/show'
-  get 'homes/top'
+  root to: 'homes#top'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :books, only: [:index, :show]
+  resources :users, only: [:show, :edit]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2022_04_27_221009) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.string "body"
+    t.text "body"
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -27,9 +27,9 @@ ActiveRecord::Schema.define(version: 2022_04_27_221009) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
+    t.text "introduction"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
