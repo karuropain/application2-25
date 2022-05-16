67efe1033bead78fd5a7960aa5738e49f7085a8f
karuropain <fs.sai5041@gmail.com>
2022-05-16 10:01:50 +0000

    Update footerレイアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
+  resources :homes, only: [:top, :about]
   resources :books, only: [:index, :show]
   resources :users, only: [:show, :edit]
 end
