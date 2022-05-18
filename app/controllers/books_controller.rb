d5b5920d0e9bd0a0c17555b048d4c99faac1e386
karuropain <fs.sai5041@gmail.com>
2022-05-18 10:13:22 +0000

    Update 投稿機能作成中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,26 @@
 class BooksController < ApplicationController
-  def index
+  def new
+    @book = Book.new
   end
-
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(current_user.id)
+  end
+  
+  def index
+    
+  end
+  
   def show
   end
+  
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get 'home/about', to: 'homes#about', as: 'homes_about'
   devise_for :users
   resources :books, only: [:index, :show]
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:index, :show, :edit]
 end
