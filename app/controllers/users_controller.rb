0bba8e78798032495b016e4cfb0510ec2dd4e2d3
karuropain <fs.sai5041@gmail.com>
2022-05-25 10:18:14 +0000

    Update userinfo画像表示から

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,35 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def index
+    @users = User.all
+  end
+  
+  def create
+    @user = User.new(user_params)
+    @user_id = current_user.id
+    @user.save
+  end
+  
   def show
     @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(current_user.id)
+  end
+  
+  private
+    def user_params
+      params.require(:user).permit(:image)
+    end
+  
 end
